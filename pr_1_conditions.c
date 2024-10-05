8eea0b03c1a97d8790a448315e5f744a7d9bc3c0
Shushanik Tovmasyan <shushaniktovm@gmail.com>
2024-10-06 01:16:16 +0400

    Added condition checking first problem solution.

---
+++ pr_1_conditions.c
@@ -0,0 +1,54 @@
+/******************************************************************************
+
+На входе мы получаем целые числа input_1, input_2 и input_3. Произносите "Condition is correct.", если:
+а) все три положительны,
+б) хотя бы один из них положителен,
+в) ровно один из них положителен,
+г) ровно два из них положительны,
+д) хотя бы два из них равны,
+В противном случае "Condition is not correct.".
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int input_1, input_2, input_3;
+    scanf("%d %d %d", input_1, input_2, input_3);
+    
+    if (input_1 > 0 && input_2 > 0 && input_3 > 0) {
+        printf("Condition а is not correct.");
+    }
+    
+    if (input_1 > 0 || input_2 > 0 || input_3 > 0) {
+        printf("Condition б is not correct.");
+    }
+
+    if (
+        (input_1 > 0 && input_2 < 0 && input_3 < 0) ||
+        (input_1 < 0 && input_2 > 0 && input_3 < 0) ||
+        (input_1 < 0 && input_2 < 0 && input_3 > 0)
+    ) {
+        printf("Condition в is not correct.");
+    }
+    
+    // ровно два из них положительны - то же самое что
+    // ровно один из них отрицателен
+    if (
+        (input_1 < 0 && input_2 > 0 && input_3 > 0) ||
+        (input_1 > 0 && input_2 < 0 && input_3 > 0) ||
+        (input_1 > 0 && input_2 > 0 && input_3 < 0)
+    ) {
+        printf("Condition г is not correct.");
+    }
+    
+    if(
+        (input_1 == input_2) ||
+        (input_2 == input_3) ||
+        (input_1 == input_3)
+    ) {
+        printf("Condition д is not correct.");
+    }
+
+    return 0;
+}
